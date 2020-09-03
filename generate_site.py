d88ec04f5716f5154f582d984e42acaea93b70ed
hen <dev@flamefew.com>
2020-09-02 23:44:54 -0700

    Adjusting directory names for faqxml files

--- generate_site.py
+++ generate_site.py
@@ -232,9 +232,9 @@ def load_faqs_to_dict(faq_dir, faq_dict):
         faq_db[filename] = faq_node
 
 faq_db = OrderedDict()
-load_faqs_to_dict('faqxml-official', faq_db)
-load_faqs_to_dict('faqxml-roundups', faq_db)
-load_faqs_to_dict('faqxml-unofficial', faq_db)
+load_faqs_to_dict('faqxml-wotc-faqs', faq_db)
+load_faqs_to_dict('faqxml-wotc-roundups', faq_db)
+#load_faqs_to_dict('faqxml-aequitas-roundups', faq_db)
 
 # Load the previous json index for use in hyperlinking
 faq_index_filename = os.path.join(TOP_OUTPUT_DIR, "faqindex.json")
